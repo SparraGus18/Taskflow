6980553b17b823cdc00397331e22bf38fb5f900e
SparraGus18 <jesus2411053@hybridge.education>
2025-10-21 07:10:16 +0400

    Codigo de creacion de tareas

--- src/database.py
+++ src/database.py
@@ -9,22 +9,76 @@ def get_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
-
-
-
-def crear_tarea(self, tarea: Tarea): -> Tarea:
+def crear_tabla():
     conn = get_connection()
     cursor = conn.cursor()
 
+    #tabla de proyectos
     cursor.execute("""
-        INSERT INTO tareas (titulo, descripcion, fecha_creacion, fecha_limite, prioridad, estado, proyecto_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, (tarea._titulo, tarea._descripcion, tarea._fecha_creacion, tarea._fecha_limite, tarea._prioridad, tarea._estado, tarea._proyecto_id))
+        CREATE TABLE IF NOT EXISTS proyectos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                descripcion TEXT,
+                fecha_inicio TEXT,
+                estado TEXT
+        )""")
 
+    #Tabla de tareas
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tareas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            descripcion TEXT,
+            fecha_creacion TEXT,
+            fecha_limite TEXT,
+            prioridad TEXT,
+            estado TEXT,
+            proyecto_id INTEGER NOT NULL,
+            FOREIGN KEY (proyecto_id) REFERENCES proyectos (id)
+        )
+    """)
 
+    try:
+        cursor.execute(f"INSERT INTO proyectos (id, nombre, descripcion, estado) VALUES (0, 'Tareas Generales', 'Tareas sin clasificar', 'Activo')")
+    except sqlite3.IntegrityError:
+        pass
 
-    tarea.id = cursor.lastrowid
     conn.commit()
     conn.close()
-    return tarea
+    
+
+
+class DBManager:
+
+    def __init__(self):
+        crear_tabla()
+
+    def crear_tarea(self, tarea: Tarea) -> Tarea:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO tareas (titulo, descripcion, fecha_creacion, fecha_limite, prioridad, estado, proyecto_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (tarea._titulo, tarea._descripcion, tarea._fecha_creacion, tarea._fecha_limite, tarea._prioridad, tarea._estado, tarea._proyecto_id))
+
+
+
+        tarea.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return tarea
+
+    def obtener_proyectos(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM proyectos")
+        filas = cursor.fetchall()
+        conn.close()
+
+        proyectos = [
+            Proyecto(nombre=fila['nombre'], descripcion=fila['descripcion'], id=fila['id'], estado=fila['estado']) 
+            for fila in filas
+        ]
+        return proyectos
+
